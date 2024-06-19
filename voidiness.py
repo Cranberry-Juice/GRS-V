e8fbb9611e693ea5bcf16e13a9586fa7cad4f2ca
Cranberry-Juice <josepfamador2000@gmail.com>
2024-06-18 19:28:14 -0700

    exported better instance of cel_obj data. copied and pasted voidiniss portion from analysis ipynb

--- voidiness.py
+++ voidiness.py
@@ -2,10 +2,10 @@
 from custom_functions import *
 import pandas as pd
 
-
-# Strictly Sticking to voidiness analysis we only need  positional data 'RAJ2000', 'DEJ2000', 'Redshift'
 CEL_DATA_FN = "cel_obj_table.xlsx"
+# Strictly Sticking to voidiness analysis we only need  positional data 'RAJ2000', 'DEJ2000', 'Redshift'
 
+VOIDS_DATA_FN = "processed_voids.xlsx"
 # Needs columns 'ID', 'RAdeg', 'DEdeg', 'z', 'Reff', 'cmvd_Mpc', 'Reff_Mpc', 'r_ang_deg'
 # - ID: Void ID from catalog
 # - RAdeg: Right Ascension in degrees in J2000 Frame
@@ -15,7 +15,7 @@ CEL_DATA_FN = "cel_obj_table.xlsx"
 # - Reff_Mpc: Effective on-sky effective radius " "
 # - r_ang_deg: Void angular radius in degrees
 
-VOIDS_DATA_FN = "processed_voids.xlsx"
+
 
 
 
