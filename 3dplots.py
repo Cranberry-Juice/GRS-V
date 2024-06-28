4fa582e26b5bcfc5bf90a503443b98b34278831a
Cranberry-Juice <josepfamador2000@gmail.com>
2024-06-28 14:04:37 -0700

    Scaled the void comoving distance and radii to the most distant void

--- 3dplots.py
+++ 3dplots.py
@@ -1,5 +1,12 @@
 import plotly.express as px
-df = px.data.iris()
-fig = px.scatter_3d(df, x='sepal_length', y='sepal_width', z='petal_width',
-              color='species')
-fig.show()
+import pandas as pd
+
+
+# Read in void data
+voids = pd.read_excel('exported_dataFrames/voids.xlsx')
+
+# Scale the void moving distance to be between 0 and 1
+unit = max(voids.cmvd_Mpc)
+voids.cmvd_Mpc = voids.cmvd_Mpc/unit
+# Scale the radii as well
+voids.Reff_Mpc = voids.Reff_Mpc/unit
