31dce2018c994a734c4cde902381a7000058e671
Cranberry-Juice <josepfamador2000@gmail.com>
2024-07-09 16:02:05 -0700

    recovered index data from perimeter voids and now plotting perimeter voids directly from main void table

--- perimeter.py
+++ perimeter.py
@@ -3,6 +3,7 @@ from descartes import PolygonPatch # comes with bug. See NOTE below.
 from matplotlib import pyplot as plt
 import pandas as pd
 import alphashape # used to determine the outer perimeter of a set of points
+import numpy as np
 
 
 voids = pd.read_excel('exported_dataFrames/voids.xlsx')
@@ -15,7 +16,17 @@ for ra, de in zip(voids.RAdeg, voids.DEdeg):
 
 # Generate the alphashape
 alpha = 0.20 # tightness of the shape. Higher is tighter. To high and data points are lost
-a_shape = alphashape.alphashape(v_coords, alpha=alpha)
+
+def variable_alpha(indices, r):
+    top_alpha = 0.2 # alpha value for the top of the data points
+    bottom_alpha = 0.4
+    # filter by declination. Points with DE higher than 20 dec get top alpha
+    # lower than 20 dec gets bottom alpha
+    cutoff_de = 20 #deg
+
+    return top_alpha if any(np.array(v_coords)[indices][:, 1] > cutoff_de) else bottom_alpha
+
+a_shape = alphashape.alphashape(v_coords, variable_alpha)
 fig, ax = plt.subplots()
 ax.scatter(*zip(*v_coords), marker='.')
 
@@ -34,5 +45,20 @@ ax.set_title('Cosmic Void Centers and Perimeter')
 
 # Mark the void centers that define the boundary
 ra, de = a_shape.exterior.coords.xy
-plt.scatter(ra.tolist(), de.tolist(), marker='.', color= 'red')
+ra = ra.tolist()
+de = de.tolist()
+
+# We have raw RA, DE but we need to know what voids they are to get radius information
+maskra = voids.isin(ra).RAdeg
+maskde = voids.isin(de).DEdeg
+
+# Combine the mask. len(mask) < len(ra) by one. This is expected since the first
+# ra, dec pair is also the last to define the polygon. 
+# AKA fence post error, though not an error in this case.
+mask = [x and y for x, y in zip(maskra, maskde)]
+
+idx = voids[mask].index # indices of voids
+
+# plot in a different color to confirm
+ax.scatter(voids.loc[idx, "RAdeg"], voids.loc[idx, "DEdeg"], marker=".", color = 'red')
 plt.show()
