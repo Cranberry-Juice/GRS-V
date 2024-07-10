5aed573e96e877b5d40a613b26b01bdd2603b8f4
Cranberry-Juice <josepfamador2000@gmail.com>
2024-07-10 14:56:58 -0700

    Exported Gamma Ray Sources Table before any filtering is applied

--- footprint_filter.py
+++ footprint_filter.py
@@ -1,6 +1,12 @@
-"""Determines wither a set of points falls inside a polygon defined by another
+"""Determines wether a set of points falls inside a polygon defined by another
 set of points. In the context of this research, determines if a celestial
 object falls in the void catalog footprint"""
 
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
+import pandas as pd
+
+
+voids = pd.read_excel('exported_dataFrames/voids.xlsx')
+
+cel_obj = pd.read_excel()
